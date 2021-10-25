bc784258ed7daa1284fc5a0da36b7c59efb0264a
Maulia Artadianti <mauliadianti9@gmail.com>
2021-10-25 22:23:14 +0700

    make it better?

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const port = 3000
+const port = process.env.PORT || 3000
 const path = require('path')
 const dotenv = require('dotenv')
 dotenv.config()
